1394109fd77461a93bd4a20d875cf759888ed42b
dev-peak1217 <devpeak1217@gmail.com>
2026-03-15 18:08:19 +0900

    프로젝트 등록

---
+++ src/test/java/study/drivendevelopment/DrivenDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package study.drivendevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivenDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
